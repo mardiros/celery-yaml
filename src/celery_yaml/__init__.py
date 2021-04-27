06ba2f4c2812e9fa8d7e5aff99b19aebc2f6edc5
Guillaume Gauvrit <guillaume@gauvr.it>
2021-04-27 19:31:58 +0200

    Fix load of the app

--- src/celery_yaml/__init__.py
+++ src/celery_yaml/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
-from .loader import YamlLoader
+from .loader import add_yaml_option
 from .pyramid import includeme
 
 __version__ = pkg_resources.get_distribution("celery-yaml").version
--- src/celery_yaml/loader.py
+++ src/celery_yaml/loader.py
@@ -1,14 +1,71 @@
 """Helper for celery."""
-
 import logging
 import os
+import sys
 from logging.config import dictConfig
 
 import yaml
+from celery import VERSION as celery_version
 import celery.loaders.base
 from celery import Celery
 
 log = logging.getLogger(__name__)
+
+
+if celery_version.major < 5:
+
+    from celery.signals import user_preload_options
+
+    @user_preload_options.connect
+    def on_preload_parsed(options, **kwargs):
+        config_path = options["yaml"]
+        app = kwargs['app']
+
+        if config_path is None:
+            print("You must provide the --yaml argument")
+            sys.exit(-1)
+
+        try:
+            loader = YamlLoader(app, config=config_path)
+            loader.read_configuration()
+        except Exception as err:
+            print(err)
+            sys.exit(-1)
+
+
+def add_yaml_option(app):
+
+    help = "Celery configuration in a YAML file."
+    if celery_version.major < 5:
+
+        def add_preload_arguments(parser):
+            parser.add_argument(
+                "--yaml", default=None, help=help
+            )
+        app.user_options["preload"].add(add_preload_arguments)
+
+    else:
+
+        from click import Option
+        from celery import bootsteps
+
+        app.user_options['preload'].add(
+            Option(['--yaml'], required=True, help=help)
+        )
+
+        class YamlBootstep(bootsteps.Step):
+
+            def __init__(self, parent, yaml: str="", **options):
+                try:
+                    loader = YamlLoader(app, config=yaml)
+                    loader.read_configuration()
+                    loader.import_default_modules()
+                except Exception as err:
+                    print(err)
+                    sys.exit(-1)
+                super().__init__(parent, **options)
+
+        app.steps['worker'].add(YamlBootstep)
 
 
 class YamlLoader(celery.loaders.base.BaseLoader):
@@ -33,4 +90,4 @@ class YamlLoader(celery.loaders.base.BaseLoader):
             self.app.config_from_object(_conf["celery"])
             if self.configure_logging and "logging" in _conf:
                 dictConfig(_conf["logging"])
-        return _conf.get("app", {})
+        return _conf["celery"]
