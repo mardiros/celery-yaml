7da37ad4ac84360596a1268407126d5f906cf399
Guillaume Gauvrit <guillaume@gauvr.it>
2023-02-02 16:54:19 +0100

    Add missing script

---
+++ scripts/write_changelog.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import datetime
+
+import celery_yaml
+
+header = (
+    f"## {celery_yaml.__version__} - "
+    f"Released on {datetime.datetime.now().date().isoformat()}"
+)
+with open("CHANGELOG.md.new", "w") as changelog:
+    changelog.write(header)
+    changelog.write("\n")
+    changelog.write("* please write here \n")
