61fd2afc10723a1a19d4718e68f881e45afd6ba8
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-27 17:30:10 +0100

    Use imporlib.metadata

--- src/celery_yaml/__init__.py
+++ src/celery_yaml/__init__.py
@@ -1,9 +1,9 @@
-import pkg_resources
+import importlib.metadata
 
 from .loader import add_yaml_option
 from .pyramid import includeme
 
-__version__ = pkg_resources.get_distribution("celery-yaml").version
+__version__ = importlib.metadata.version("plaster-yaml")
 
 __all__ = [
     "add_yaml_option",
