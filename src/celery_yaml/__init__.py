d3b8b201ade3a28d5c8c3d585b1eaaf1a467630b
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-27 18:26:15 +0100

    Fix version number

--- src/celery_yaml/__init__.py
+++ src/celery_yaml/__init__.py
@@ -3,7 +3,7 @@ import importlib.metadata
 from .loader import add_yaml_option
 from .pyramid import includeme
 
-__version__ = importlib.metadata.version("plaster-yaml")
+__version__ = importlib.metadata.version("celery-yaml")
 
 __all__ = [
     "add_yaml_option",
